bf6e41b6465fb435c68e8e38fd99945816850c53
Matheus Avellar <matheusavellarb@yahoo.com>
2015-03-29 21:50:47 -0300

    Added a bit of an ajax check, hopefully it'll work

--- jayess.js
+++ jayess.js
@@ -22,6 +22,7 @@ function beforeStart(){
 	}
 }
 function start(){
+	$("body").hover(function(){console.log("Ajax is working");});
 	hasLoaded = true;
 	var bg_colour = Math.floor(Math.random() * 16777215).toString(16);
 	bg_colour = "#" + ("000000" + bg_colour).slice(-6);
