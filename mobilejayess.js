b0481a212d72a5d0480b9217c67157ba42eb8a00
Matheus Avellar <matheusavellarb@yahoo.com>
2015-03-30 22:17:15 -0300

    Updated some of Mobile version

--- mobilejayess.js
+++ mobilejayess.js
@@ -2,5 +2,4 @@ function start(){
 	var bg_colour = Math.floor(Math.random() * 16777215).toString(16);
 	bg_colour = "#" + ("000000" + bg_colour).slice(-6);
 	document.bgColor = bg_colour;
-	$("#title").text(BD.OS + " | " + BD.browser);
 }
