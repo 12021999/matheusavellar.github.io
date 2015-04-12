f73b60ab79c5e06b32d72fafc80b8773ff57b582
Matheus Avellar <matheusavellarb@yahoo.com>
2015-04-12 16:39:29 -0300

    Added color ;O

--- jayess.js
+++ jayess.js
@@ -29,8 +29,6 @@ function beforeStart(){
 function start(){
 	$("body").hover(function(){/*AJAX TEST*/});
 	hasLoaded = true;
-	var bg_colour = Math.floor(Math.random() * 16777215).toString(16);
-	bg_colour = "#" + ("000000" + bg_colour).slice(-6);
 	document.bgColor = bg_colour;
 
 	$("#refresh").mouseup(function() {
